c7bba4b7e734e7a42f16991a5c58d558b7dc8b22
James Booth <james.booth@microchip.com>
2022-09-27 15:33:59 +0100

    testing db.py

--- blog/db.py
+++ blog/db.py
@@ -4,13 +4,10 @@ from sqlalchemy import create_engine
 def get_engine(Server, Database, Driver):
         #SQL Connection Windows Authentication#
 
-        Server = 'UKC-VM-SQL01'
-        Database = 'Stencil'
-        Driver = 'ODBC Driver 17 for SQL Server'
         Database_con = f'mssql://@{Server}/{Database}?driver={Driver}'
 
         engine = create_engine(Database_con)
-        con = engine.connect()
+        return engine
 
 
         #df.to_sql('StencilUsage', con, if_exists='append', index = False)
